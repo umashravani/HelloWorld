283f56b763bae4b6956953ce08d240a131a862cf
Umashravani Buddharaju <Umashravani.buddharaju@adudev.com>
2019-10-15 09:09:15 +1100

    Added BingSearchTest, Selenium  Nuget, search in Bing website , Impemented init, Moved  test initialize and clean up into a different class

---
+++ Bing/Tests/BingSearchTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using Bing.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Bing
+{
+    [TestClass]
+     public class BingSearchTest: Differentclass
+    {
+        //public IWebDriver driver;
+        [TestInitialize]
+        public void TestFirst()
+        {
+            BingSearchTest call = new BingSearchTest();
+            call.TestInit();
+        }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+            string actualtitle = driver.Title;
+            String expectedTitle = "";
+        //  Assert(actualtitle== expectedTitle);
+            if (actualtitle.Contains(expectedTitle))
+            {
+                Console.WriteLine("Title is matching with expected value");
+            }
+            else
+            {
+                Console.WriteLine("Title is not matching with expected value");
+            }
+
+
+
+
+        }
+    [TestCleanup]
+        public void TestClean()
+        {
+           
+            driver.Quit();
+        }
+    }
+}
---
+++ Bing/Tests/Differentclass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bing.Tests
+{
+   public class Differentclass
+    {
+        public IWebDriver driver;
+        [TestInitialize]
+        public void TestInit()
+        {
+            driver = new ChromeDriver(@"C:\Selenium");
+            driver.Navigate().GoToUrl("https://www.bing.com/");
+        }
+    }
+}
